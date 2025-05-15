b2cc74057684f34b6a97b7047997f97b64d33bad
Esteban Vasco <evasco.2@hotmail.com>
2025-05-14 20:11:16 -0500

    Add country services and interface

---
+++ WebApplication/WebApplication1/Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.DAL.Entities;
+
+namespace WebApplication1.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+
+        Task<IEnumerable<Country>> GetCountriesAsync();
+        Task<Country> GetCountryByIdAsync(Guid id);
+        Task<Country> CreateCountryAsync(Country country);
+        Task<Country> UpdateCountryAsync(Country country);
+        Task<Country> DeleteCountryAsync(Guid id);
+    }
+}
---
+++ WebApplication/WebApplication1/Domain/Services/CountryService.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication1.Domain.Services
+{
+    public class CountryService
+    {
+    }
+}
